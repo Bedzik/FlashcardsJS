738379a1d628ef047dbff02ca82078ccdb81133e
Bedzik <lukaszbednarski63@gmail.com>
2020-10-10 19:12:43 +0200

    buttons and card event handling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 var good = document.getElementById('good');
 var card = document.getElementById('card');
+var bad = document.getElementById('bad');
+var lack = document.getElementById('lack');
 
 function test() {
     alert(good);
-} 
-good.onclick = function () {
+}
+good.onclick = function() {
     card.innerHTML = "Samochód";
     //test();
-} 
+}
+bad.onclick = function() {
+    card.innerHTML = "Samochód";
+}
+lack.onclick = function() {
+    card.innerHTML = "Samochód";
+}
